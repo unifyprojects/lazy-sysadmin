16655d1952781aeaa1d050b88ce19d2d564b1216
azjezz <29315886+azjezz@users.noreply.github.com>
2018-12-31 18:53:38 +0100

    Update lazy-sysadmin.php

--- bin/lazy-sysadmin.php
+++ bin/lazy-sysadmin.php
@@ -5,7 +5,11 @@ use League\CLImate\CLImate;
 require_once __DIR__.'/../vendor/autoload.php';
 
 $console = new CLImate();
-$console->clear();
+
+$console->clear(); // clear window
 $console->green('lazy-sysadmin version 0.2');
+$console->br(); // new line
+$console->border(); // border
+$console->br(); // new line
 
 LazySysAdmin\main($console);
